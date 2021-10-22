e68e41e440d8734ba706ccc1fabb6094bc7f6846
Santamaria Paul <psantamaria@oui.sncf>
2021-10-22 11:30:53 +0200

    Tailwind working with React

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Button = () => {
+    return <button className={"bg-blue-500"}>Hello WOrd</button>
+}
---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Button } from './Button';
+
+
+export default {
+    title: "Component/Button",
+    component: Button,
+};
+
+export const Default = (args) => {
+    return <Button />
+};
